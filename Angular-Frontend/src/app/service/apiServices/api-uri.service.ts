1f4eedf7cbb2ccc184a5f24dad822b98ee52d508
Golanukonda Sai kumar <61460608+tronkiller1919@users.noreply.github.com>
2022-08-17 20:00:13 +0530

    Update api-uri.service.ts

--- Angular-Frontend/src/app/service/apiServices/api-uri.service.ts
+++ Angular-Frontend/src/app/service/apiServices/api-uri.service.ts
@@ -7,14 +7,14 @@ export class ApiUriService {
   constructor() {}
 
   readonly SIGNUP_API_URI: string =
-    "http://ec2-18-233-29-224.compute-1.amazonaws.com/auth/signup";
+    "http://localhost:8991/auth/signup";
 
   readonly LOGIN_API_URI: string =
-    "http://ec2-18-233-29-224.compute-1.amazonaws.com/auth/login";
+    "http://localhost:8991/auth/login";
 
   readonly PROCESS_COMPONENT_API_URI: string =
-    "http://ec2-18-233-29-224.compute-1.amazonaws.com/component-process/process-request";
+    "http://localhost:8991/component-process/process-request";
 
   readonly CONFIRM_ORDER_API_URI: string =
-    "http://ec2-18-233-29-224.compute-1.amazonaws.com/component-process/complete-processing";
+    "http://localhost:8991/component-process/complete-processing";
 }
